64189f1572bddf7cd806c14844ae334acfd71d4c
fabio21777 <fabiosouza21777@gmail.com>
2021-10-31 14:43:57 -0300

    ajustanto arquivo yaml

--- order/src/main/java/com/fsm/order/clientHttp/CartClientHttp.java
+++ order/src/main/java/com/fsm/order/clientHttp/CartClientHttp.java
@@ -9,7 +9,7 @@ import com.fsm.order.external.entities.carts.Cart;
 
 
 
-@FeignClient(name = "cart-service", url = "localhost:9000")
+@FeignClient(name = "cart-service", url = "https://shopping-cart-tr.herokuapp.com")
 public interface CartClientHttp {
 	@GetMapping(value = "/carts/{id}" )
 	public Cart getCart(@PathVariable("id") String id);
--- order/src/main/java/com/fsm/order/clientHttp/CredentialClienteHttp.java
+++ order/src/main/java/com/fsm/order/clientHttp/CredentialClienteHttp.java
@@ -6,7 +6,7 @@ import org.springframework.web.bind.annotation.PathVariable;
 
 import com.fsm.order.external.entities.credentials.DtoUser;
 
-@FeignClient(name = "credential-service", url = "localhost:8000")
+@FeignClient(name = "credential-service", url = "https://credentials-tr.herokuapp.com")
 public interface CredentialClienteHttp {
 	@GetMapping(value = "/users/{id}" )
 	public DtoUser getUser(@PathVariable("id") String id);
